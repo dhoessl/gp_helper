2700ed4f90ec046e8f5605223f504f10ee04e722
Dominic Hößl <dominic.hoessl@noris.de>
2024-08-30 22:42:46 +0200

    jsdelivr is now proxying the data for the script

--- gp_ally_stat_display_tampermonkey.js
+++ gp_ally_stat_display_tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Grepo Alliance Member State
 // @namespace     dhoessl_grepo
-// @version       0.1.2
+// @version       0.1.3
 // @author        dhoessl
 // @homepage      https://github.com/dhoessl/gp_helper/
 // @updateURL     https://raw.githubusercontent.com/dhoessl/gp_helper/master/gp_ally_stat_display_tampermonkey.js
@@ -15,7 +15,8 @@
 (function() {
   var JSscript = document.createElement("script");
   JSscript.type = 'text/javascript';
-  JSscript.src = "https://raw.githubusercontent.com/dhoessl/gp_helper/master/gp_ally_stat_display.js";
+  // Content needs to be delivered from jsdelivr because github sending out wronge Mime Type :/
+  JSscript.src = "https://cdn.jsdelivr.net/gh/dhoessl/gp_helper/gp_ally_stat_display.js";
   document.getElementsByTagName("head")[0].appendChild(JSscript);
 })();
 
